d637fcd52ca88a7cbf8838cce1a479b9613946c2
David Williams <thedavesignal@gmail.com>
2017-09-11 16:16:45 -0700

    Implement build set to populate side of board method is called on

--- lib/set.rb
+++ lib/set.rb
@@ -1,32 +1,66 @@
 require "piece"
 class Set
-  attr_accessor :traits
+  attr_accessor :data
 
   def initialize(color, parent)
     @color = color
     @parent = parent
-    @traits = Hash.new
+    @data = Hash.new
   end
 
+  # Populates the board, placing pieces based on color and type
   def build_set
+    if @color == :white
+      # Rooks
+      place_piece([1,8],@color, :rook)
+      place_piece([8,8],@color, :rook)
+      # Knights
+      place_piece([2,8],@color, :knight)
+      place_piece([7,1],@color, :knight)
+      # Bishops
+      place_piece([3,8],@color, :bishop)
+      place_piece([6,8],@color, :bishop)
+      # King
+      place_piece([4,8],@color, :king)
+      # Queen
+      place_piece([5,8],@color, :queen)
+      # Pawns
+      place_piece([1,7],@color,:pawn)
+      place_piece([2,7],@color,:pawn)
+      place_piece([3,7],@color,:pawn)
+      place_piece([4,7],@color,:pawn)
+      place_piece([5,7],@color,:pawn)
+      place_piece([6,7],@color,:pawn)
+      place_piece([7,7],@color,:pawn)
+      place_piece([8,7],@color,:pawn)
+    else
+      # Rooks
+      place_piece([1,1],@color, :rook)
+      place_piece([8,1],@color, :rook)
+      # Knights
+      place_piece([2,1],@color, :knight)
+      place_piece([7,1],@color, :knight)
+      # Bishops
+      place_piece([3,1],@color, :bishop)
+      place_piece([6,1],@color, :bishop)
+      # King
+      place_piece([4,1],@color, :king)
+      # Queen
+      place_piece([5,1],@color, :queen)
+      # Pawns
+      place_piece([1,2],@color,:pawn)
+      place_piece([2,2],@color,:pawn)
+      place_piece([3,2],@color,:pawn)
+      place_piece([4,2],@color,:pawn)
+      place_piece([5,2],@color,:pawn)
+      place_piece([6,2],@color,:pawn)
+      place_piece([7,2],@color,:pawn)
+      place_piece([8,2],@color,:pawn)
+    end
   end
 
-  def place_pawn
-  end
-
-  def place_rook
-  end
-
-  def place_bishop
-  end
-
-  def place_knight
-  end
-
-  def place_king
-  end
-
-  def place_queen
+  def place_piece(location, color, piece)
+    @data[location] = Piece.new(color, piece, self)
   end
 
   def capture_piece
--- spec/set_spec.rb
+++ spec/set_spec.rb
@@ -1,46 +1,54 @@
 require "set"
 describe Set do
-  context "When created" do
-    before(:each) do
-      @white_set = Set.new(:white)
+  describe "#initialize" do
+    it "should create an empty Hash 'data'" do
+      @white_set = Set.new(:white, self)
+      expect(@white_set.data.keys).to eql([])
+      expect(@white_set.data.values).to eql([])
     end
-    it "should create an empty Hash 'traits'" do
-      expect(@white_set.traits.keys).to eql([])
-      expect(@white_set.traits.values).to eql([])
+  end
+  context "should respond to" do
+    before(:all) do
+      @white_set = Set.new(:white, self)
+    end
+    it ":build_set" do
+      expect(@white_set).to respond_to(:build_set)
     end
 
-    context "should respond to" do
-      it ":build_set" do
-        expect(@white_set).to respond_to(:build_set)
-      end
+    it ":place_piece" do
+      expect(@white_set).to respond_to(:place_piece)
+    end
 
-      it ":place_pawn" do
-        expect(@white_set).to respond_to(:build_set)
-      end
+    it ":capture_piece" do
+      expect(@white_set).to respond_to(:capture_piece)
+    end
+  end
 
-      it ":place_rook" do
-        expect(@white_set).to respond_to(:build_set)
-      end
-
-      it ":place_bishop" do
-        expect(@white_set).to respond_to(:build_set)
-      end
-
-      it ":place_knight" do
-        expect(@white_set).to respond_to(:build_set)
-      end
-
-      it ":place_king" do
-        expect(@white_set).to respond_to(:build_set)
-      end
-
-      it ":place_queen" do
-        expect(@white_set).to respond_to(:build_set)
-      end
-
-      it ":capture_piece" do
-        expect(@white_set).to respond_to(:build_set)
-      end
+  describe "#build_set" do
+    before(:all) do
+      @white_set = Set.new(:white, self)
+      @white_set.build_set
+    end
+    it "should place 16 pieces" do
+      expect(@white_set.data.keys.length).to eql(16)
+    end
+    it "should place eight pawns" do
+      expect(@white_set.data.values.count{|x| x.traits[:type] == :pawn}).to eql(8)
+    end
+    it "should place two rooks" do
+      expect(@white_set.data.values.count{|x| x.traits[:type] == :rook}).to eql(2)
+    end
+    it "should place two bishops" do
+      expect(@white_set.data.values.count{|x| x.traits[:type] == :bishop}).to eql(2)
+    end
+    it "should place two knights" do
+      expect(@white_set.data.values.count{|x| x.traits[:type] == :knight}).to eql(2)
+    end
+    it "should place one king" do
+      expect(@white_set.data.values.count{|x| x.traits[:type] == :king}).to eql(1)
+    end
+    it "should place one queen" do
+      expect(@white_set.data.values.count{|x| x.traits[:type] == :queen}).to eql(1)
     end
   end
 end
