a929427077053b6a914f0c201c6659178a5d4797
David Williams <thedavesignal@gmail.com>
2017-09-12 15:04:32 -0700

    Minor formatting change (changes color order to match rules)

--- lib/set.rb
+++ lib/set.rb
@@ -12,30 +12,7 @@ class Set
 
   # Populates the board, placing pieces based on color and type
   def build_set
-    if @color == :black
-      # Rooks
-      place_piece([1,8],@color, :rook)
-      place_piece([8,8],@color, :rook)
-      # Knights
-      place_piece([2,8],@color, :knight)
-      place_piece([7,1],@color, :knight)
-      # Bishops
-      place_piece([3,8],@color, :bishop)
-      place_piece([6,8],@color, :bishop)
-      # King
-      place_piece([4,8],@color, :king)
-      # Queen
-      place_piece([5,8],@color, :queen)
-      # Pawns
-      place_piece([1,7],@color,:pawn)
-      place_piece([2,7],@color,:pawn)
-      place_piece([3,7],@color,:pawn)
-      place_piece([4,7],@color,:pawn)
-      place_piece([5,7],@color,:pawn)
-      place_piece([6,7],@color,:pawn)
-      place_piece([7,7],@color,:pawn)
-      place_piece([8,7],@color,:pawn)
-    else
+    if @color == :white
       # Rooks
       place_piece([1,1],@color, :rook)
       place_piece([8,1],@color, :rook)
@@ -58,6 +35,29 @@ class Set
       place_piece([6,2],@color,:pawn)
       place_piece([7,2],@color,:pawn)
       place_piece([8,2],@color,:pawn)
+    else
+      # Rooks
+      place_piece([1,8],@color, :rook)
+      place_piece([8,8],@color, :rook)
+      # Knights
+      place_piece([2,8],@color, :knight)
+      place_piece([7,1],@color, :knight)
+      # Bishops
+      place_piece([3,8],@color, :bishop)
+      place_piece([6,8],@color, :bishop)
+      # King
+      place_piece([4,8],@color, :king)
+      # Queen
+      place_piece([5,8],@color, :queen)
+      # Pawns
+      place_piece([1,7],@color,:pawn)
+      place_piece([2,7],@color,:pawn)
+      place_piece([3,7],@color,:pawn)
+      place_piece([4,7],@color,:pawn)
+      place_piece([5,7],@color,:pawn)
+      place_piece([6,7],@color,:pawn)
+      place_piece([7,7],@color,:pawn)
+      place_piece([8,7],@color,:pawn)
     end
   end
 
