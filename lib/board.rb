b5da43973d48f0f8c6717a55e1670c76c0bacc22
David Williams <thedavesignal@gmail.com>
2017-09-14 16:30:45 -0700

    Implement :populate_board to populate the board with pieces

--- lib/board.rb
+++ lib/board.rb
@@ -1,15 +1,36 @@
+require_relative "set"
+# Controls the layout of the board, and what piece lies where
 class Board
   attr_reader :data
+  attr_reader :white_set
   def initialize
     @data = Hash.new
     @x_index = [nil,"a","b","c","d","e","f","g","h"]
   end
 
+  # Contructs a hash with 64 definitions to serve as the board
   def build_board
     8.times do |y|
       8.times do |x|
         @data[[x+1,y+1]] = { :proper_name => "#{@x_index[x+1]}#{y+1}"}
       end
+    end
+  end
+
+  # Calls :build_set from ::Set to build the list of pieces, then assigns them
+  def populate_board
+    @white_set = Set.new(:white, self)
+    @black_set = Set.new(:black, self)
+
+    @white_set.build_set
+    @black_set.build_set
+
+    @white_set.data.each do |key, value|
+      @data[key][:occupant] = value
+    end
+
+    @black_set.data.each do |key, value|
+      @data[key][:occupant] = value
     end
   end
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -5,6 +5,7 @@ describe Board do
   end
   it "should respond to :build_board" do
     expect(@gameboard).to respond_to(:build_board)
+    expect(@gameboard).to respond_to(:populate_board)
   end
   context ":build_board" do
     before(:each) do
@@ -32,6 +33,52 @@ describe Board do
       expect(@gameboard.data[[6,6]][:proper_name]).to eql("f6")
       expect(@gameboard.data[[7,7]][:proper_name]).to eql("g7")
       expect(@gameboard.data[[8,8]][:proper_name]).to eql("h8")
+    end
+
+
+  end
+
+  context ":populate_board" do
+    before(:each) do
+      @gameboard.build_board
+      @gameboard.populate_board
+    end
+    it "should place a white rook on a1" do
+      expect(@gameboard.data[[1,1]][:occupant].traits[:type]).to eql(:rook)
+      expect(@gameboard.data[[1,1]][:occupant].traits[:color]).to eql(:white)
+    end
+    it "should place a black rook on h8" do
+      expect(@gameboard.data[[8,8]][:occupant].traits[:type]).to eql(:rook)
+      expect(@gameboard.data[[8,8]][:occupant].traits[:color]).to eql(:black)
+    end
+    it "should place a white knight on b1" do
+      expect(@gameboard.data[[2,1]][:occupant].traits[:type]).to eql(:knight)
+      expect(@gameboard.data[[2,1]][:occupant].traits[:color]).to eql(:white)
+    end
+    it "should place a black knight on g8" do
+      expect(@gameboard.data[[7,8]][:occupant].traits[:type]).to eql(:knight)
+      expect(@gameboard.data[[7,8]][:occupant].traits[:color]).to eql(:black)
+    end
+    it "should place a white bishop on b1" do
+      expect(@gameboard.data[[3,1]][:occupant].traits[:type]).to eql(:bishop)
+      expect(@gameboard.data[[3,1]][:occupant].traits[:color]).to eql(:white)
+    end
+    it "should place a black bishop on g8" do
+      expect(@gameboard.data[[6,8]][:occupant].traits[:type]).to eql(:bishop)
+      expect(@gameboard.data[[6,8]][:occupant].traits[:color]).to eql(:black)
+    end
+    it "should place a white queen on d1" do
+      expect(@gameboard.data[[4,1]][:occupant].traits[:type]).to eql(:queen)
+      expect(@gameboard.data[[4,1]][:occupant].traits[:color]).to eql(:white)
+    end
+    it "should place a black king on e8" do
+      expect(@gameboard.data[[5,8]][:occupant].traits[:type]).to eql(:king)
+      expect(@gameboard.data[[5,8]][:occupant].traits[:color]).to eql(:black)
+    end
+
+    it "should place 16 pawns" do
+      placed_pieces = @gameboard.data.select{|k,v| v.include?(:occupant)}
+      expect(placed_pieces.select{|k,v| v[:occupant].traits[:type]==:pawn}.count).to eql(16)
     end
   end
 end
