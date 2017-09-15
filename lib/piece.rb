c8d84454f61ca651d0b72f341944067048b19784
David Williams <thedavesignal@gmail.com>
2017-09-14 17:52:20 -0700

    Refactor data storage for pieces for greater usability and clarity

--- lib/piece.rb
+++ lib/piece.rb
@@ -1,7 +1,7 @@
 
 # Controls the definition and attributes of individual pieces
 class Piece
-  attr_accessor :traits
+  attr_reader :color, :type, :owner, :short_desc, :move_data, :can_move_to, :can_capture, :capture_data, :has_moved, :has_castled, :in_check
   def initialize(color, type, owner)
     @color = color
     @type = type
@@ -13,105 +13,117 @@ class Piece
 
   # Builds the attributes of the piece based on type
   def actualize
-    @traits = Hash.new
-    @traits[:color] = @color
-    @traits[:type] = @type
-    @traits[:short_desc] = "#{@color.to_s} #{@type.to_s}"
-    @traits[:can_move_to] = []
-    @traits[:can_capture] = []
+    @short_desc = "#{@color.to_s} #{@type.to_s}"
+    @move_data = {n: [], ne: [], e: [], se: [], s: [], sw: [], w: [], nw: []}
+    @can_move_to = []
+    @can_capture = []
     self.send(@type)
   end
 
   # Contains attributes for pawns
   def pawn
-    if @traits[:color] == :white
-      @traits[:move_data] = [[0,1]]
-      @traits[:capture_data] = [[-1,1],[1,1]]
+    @capture_data = Hash.new
+    if @color == :white
+      @move_data[:n] << [0,1]
+      @capture_data[:nw] == [-1,1]
+      @capture_data[:ne] == [1,1]
     else
-      @traits[:move_data] = [[0,-1]]
-      @traits[:capture_data] = [[-1,-1],[1,-1]]
+      @move_data[:n] << [0,-1]
+      @capture_data[:sw] == [-1,-1]
+      @capture_data[:se] == [1,-1]
     end
-    @traits[:has_moved] = false
+    @has_moved = false
   end
 
   # Contains attributes for kings
   def king
-    @traits[:move_data] = [[0,1], [0,-1], [-1,1], [1,1], [-1,-1], [1,-1], [1,0], [-1,0]]
-    @traits[:capture_data] = @traits[:move_data]
-    @traits[:has_castled] = false
-    @traits[:in_check] = false
+    @move_data[:n] << [0,1]
+    @move_data[:ne] << [1,1]
+    @move_data[:e] << [1,0]
+    @move_data[:se] << [1,-1]
+    @move_data[:s] << [0,-1]
+    @move_data[:sw] << [-1,-1]
+    @move_data[:w] << [-1,0]
+    @move_data[:nw] << [-1,1]
+
+    @has_castled = false
+    @in_check = false
   end
 
   # Contains attributes for queens
   def queen
-    @traits[:move_data] = []
     # Cheaper than manually entering every permutation
     # Diagonal movement:
     for i in 1..7 do
-      @traits[:move_data] << [i, i]
+      @move_data[:ne] << [i, i]
     end
     for i in 1..7 do
-      @traits[:move_data] << [i, i * (-1)]
+      @move_data[:se] << [i, i * (-1)]
     end
     for i in 1..7 do
-      @traits[:move_data] << [i * (-1), i]
+      @move_data[:nw] << [i * (-1), i]
     end
     for i in 1..7 do
-      @traits[:move_data] << [i * (-1), i * (-1)]
+      @move_data[:sw] << [i * (-1), i * (-1)]
     end
 
     # Vertical and horizontal movement
-    for i in -7..7 do
-      next if i == 0
-      @traits[:move_data] << [0, i]
+    for i in 1..7 do
+      @move_data[:n] << [0, i]
     end
-    for i in -7..7 do
-      next if i == 0
-      @traits[:move_data] << [i, 0]
+    for i in 1..7 do
+      @move_data[:s] << [0, i * -1]
     end
-
-    @traits[:capture_data] = @traits[:move_data]
+    for i in 1..7 do
+      next if i == 0
+      @move_data[:e] << [i, 0]
+    end
+    for i in 1..7 do
+      next if i == 0
+      @move_data[:w] << [i * -1, 0]
+    end
   end
 
   # Contains attributes for rooks
   def rook
-    @traits[:move_data] = []
     # Vertical and horizontal movement
-    for i in -7..7 do
-      next if i == 0
-      @traits[:move_data] << [0, i]
+    for i in 1..7 do
+      @move_data[:n] << [0, i]
     end
-    for i in -7..7 do
-      next if i == 0
-      @traits[:move_data] << [i, 0]
+    for i in 1..7 do
+      @move_data[:s] << [0, i * -1]
     end
-
-    @traits[:capture_data] = @traits[:move_data]
-    @traits[:has_castled] = false
+    for i in 1..7 do
+      next if i == 0
+      @move_data[:e] << [i, 0]
+    end
+    for i in 1..7 do
+      next if i == 0
+      @move_data[:w] << [i * -1, 0]
+    end
+    
+    @has_castled = false
   end
 
   # Contains attributes for bishops
   def bishop
-    @traits[:move_data] = []
     # Diagonal movement:
     for i in 1..7 do
-      @traits[:move_data] << [i, i]
+      @move_data[:ne] << [i, i]
     end
     for i in 1..7 do
-      @traits[:move_data] << [i, i * (-1)]
+      @move_data[:se] << [i, i * (-1)]
     end
     for i in 1..7 do
-      @traits[:move_data] << [i * (-1), i]
+      @move_data[:nw] << [i * (-1), i]
     end
     for i in 1..7 do
-      @traits[:move_data] << [i * (-1), i * (-1)]
+      @move_data[:sw] << [i * (-1), i * (-1)]
     end
-    @traits[:capture_data] = @traits[:move_data]
   end
 
   # Contains attributes for knights, saying "Ni" optional
   def knight
-    @traits[:move_data] = [[1,2], [1,-2], [-1, 2], [-1,-2], [2,1], [2, -1], [-2, 1], [-2,-1]]
-    @traits[:capture_data] = @traits[:move_data]
+    @move_data = [[1,2], [1,-2], [-1, 2], [-1,-2], [2,1], [2, -1], [-2, 1], [-2,-1]]
   end
 end
--- spec/piece_spec.rb
+++ spec/piece_spec.rb
@@ -2,56 +2,58 @@ require "piece"
 describe Piece do
   context "when initialized" do
     context "with :white :queen" do
-      before(:each) do
-        @test = Piece.new(:white, :queen)
+      before(:all) do
+        @test = Piece.new(:white, :queen, self)
+        @moves = []
+        @test.move_data.each_value {|v| v.each {|x| @moves << x}}
       end
       it "should include the movement [1,1]" do
-        expect(@test.traits[:move]).to include([1,1])
+        expect(@moves).to include([1,1])
       end
       it "should include the movement [3,-3]" do
-        expect(@test.traits[:move]).to include([3,-3])
+        expect(@moves).to include([3,-3])
       end
       it "should include the movement [-5,5]" do
-        expect(@test.traits[:move]).to include([-5,5])
+        expect(@moves).to include([-5,5])
       end
       it "should include the movement [-7,-7]" do
-        expect(@test.traits[:move]).to include([-7,-7])
+        expect(@moves).to include([-7,-7])
       end
 
       it "should not include the movement [8,8]" do
-        expect(@test.traits[:move]).not_to include([8,8])
+        expect(@moves).not_to include([8,8])
       end
 
       it "should include the movement [0,-7]" do
-        expect(@test.traits[:move]).to include([0,-7])
+        expect(@moves).to include([0,-7])
       end
 
       it "should include the movement [0,3]" do
-        expect(@test.traits[:move]).to include([0,3])
+        expect(@moves).to include([0,3])
       end
 
       it "should include the movement [-3,0]" do
-        expect(@test.traits[:move]).to include([-3,0])
+        expect(@moves).to include([-3,0])
       end
 
       it "should include the movement [7,0]" do
-        expect(@test.traits[:move]).to include([7,0])
+        expect(@moves).to include([7,0])
       end
 
       it "should not include the movement [0,0]" do
-        expect(@test.traits[:move]).not_to include([0,0])
+        expect(@moves).not_to include([0,0])
       end
 
       it "should not include the movement [1,3]" do
-        expect(@test.traits[:move]).not_to include([1,3])
+        expect(@moves).not_to include([1,3])
       end
 
       it "should not include the movement [0,9]" do
-        expect(@test.traits[:move]).not_to include([0,9])
+        expect(@moves).not_to include([0,9])
       end
 
       it "should correctly self-identify as a white queen" do
-        expect(@test.traits[:short_desc]).to eql("white queen")
+        expect(@test.short_desc).to eql("white queen")
       end
     end
   end
